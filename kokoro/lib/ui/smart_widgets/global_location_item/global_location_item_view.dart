1292b7c20c634c1b6f6d896ccc89041f16cbc1e5
Julian <jfkback@gmail.com>
2021-03-12 10:49:18 -0500

    Added clickable locations to Global View

--- kokoro/lib/ui/smart_widgets/global_location_item/global_location_item_view.dart
+++ kokoro/lib/ui/smart_widgets/global_location_item/global_location_item_view.dart
@@ -1,45 +1,101 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:kokoro/ui/smart_widgets/global_location_item/global_location_item_viewmodel.dart';
 import 'package:stacked/stacked.dart';
+import 'package:vector_math/vector_math.dart' hide Colors;
 
 class GlobalLocationItemView extends StatelessWidget {
-  const GlobalLocationItemView({Key key}) : super(key: key);
+  const GlobalLocationItemView({Key key, this.intensity, this.zoom, this.location, this.mapCallback})
+      : super(key: key);
+
+  final double intensity;
+  final double zoom;
+  final String location;
+  final Function mapCallback;
 
   @override
   Widget build(BuildContext context) {
+    print('In location zoom: ${zoom} ${(1 - ((16.0 - zoom) / 15)) * 300.0}');
     return ViewModelBuilder<GlobalLocationItemViewModel>.reactive(
-      builder: (context, model, child) => !model.isClicked
-          ? GestureDetector(
-              child: Container(
-                width: 60,
-                height: 60,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Color(0xFFe0f2f1),
-                ),
-              ),
-              onTap: () {
-                model.clicked();
-              },
-            )
-          : Stack(
-              children: [
-                GestureDetector(
+      builder: (context, model, child) {
+        if (zoom < 5 && model.isClicked) {
+          model.isClicked = false;
+        }
+        return !model.isClicked
+            ? GestureDetector(
+                child: Center(
                   child: Container(
-                    width: 60,
-                    height: 60,
+                    width: (1 - ((16.0 - zoom) / 15)) * 100.0,
+                    height: (1 - ((16.0 - zoom) / 15)) * 100.0,
                     decoration: BoxDecoration(
                       shape: BoxShape.circle,
-                      color: Colors.red,
+                      color:
+                          HSVColor.fromAHSV(1.0, 211.0, intensity / 100.0, 1.0)
+                              .toColor(),
                     ),
                   ),
-                  onTap: () {
-                    model.clicked();
-                  },
-                )
-              ],
-            ),
-      viewModelBuilder: () => GlobalLocationItemViewModel(),
+                ),
+                onTap: () {
+                  print('In item, location:${location}');
+                  model.clicked(mapCallback);
+
+                },
+              )
+            : Stack(
+                children: getChildren(model, location),
+              );
+      },
+      viewModelBuilder: () => GlobalLocationItemViewModel()..setZoom(zoom),
     );
+  }
+
+  List<Widget> getChildren(model, location) {
+    int n = 1;
+    double alpha = 137.5;
+    double c = 1.0;
+    List<Widget> children = model.getProfileData(location).map<Widget>((element) {
+      double rad = c * sqrt(n);
+      double angle = n * alpha;
+
+      double x = degrees(cos(radians(angle))) * rad + 260;
+      double y = degrees(sin(radians(angle))) * rad + 320;
+
+      n++;
+      return Positioned(
+        top: (((16.0 - zoom) / 15)) * y,
+        left: (((16.0 - zoom) / 15)) * x,
+        child: Container(
+          width: (1 - ((16.0 - zoom) / 15)) * 200.0,
+          height: (1 - ((16.0 - zoom) / 15)) * 200.0,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.red,
+          ),
+          child: CircleAvatar(
+              radius: 20,
+              backgroundImage: NetworkImage(element['url'])
+          ),
+        ),
+      );
+    }).toList();
+    children.add(GestureDetector(
+      child: Center(
+        child: Container(
+          width: (1 - ((16.0 - zoom) / 15)) * 100.0,
+          height: (1 - ((16.0 - zoom) / 15)) * 100.0,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color:
+                HSVColor.fromAHSV(1.0, 211.0, intensity / 100.0, 1.0).toColor(),
+          ),
+        ),
+      ),
+      onTap: () {
+        print('In item, location:${location}');
+        model.clicked(mapCallback);
+      },
+    ));
+    return children;
   }
 }
--- kokoro/lib/ui/smart_widgets/global_map/global_map_viewmodel.dart
+++ kokoro/lib/ui/smart_widgets/global_map/global_map_viewmodel.dart
@@ -1,3 +1,20 @@
+import 'package:latlong/latlong.dart';
+import 'package:kokoro/app/app.locator.dart';
 import 'package:stacked/stacked.dart';
+import 'package:kokoro/core/services/global_map_service.dart';
 
-class GlobalMapViewModel extends BaseViewModel {}
+class GlobalMapViewModel extends BaseViewModel {
+  final _globalMapService = locator<GlobalMapService>();
+  double zoom = 2.5;
+  LatLng center = LatLng(0.0, -30.0);
+
+  List<Map> getData(zoomLevel) {
+    return _globalMapService.getData(zoomLevel);
+  }
+
+  void updateMapView() {
+    center = LatLng(50.0, -30.0);
+    zoom = 10;
+    notifyListeners();
+  }
+}
